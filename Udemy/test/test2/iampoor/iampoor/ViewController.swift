9ca6d2e81b9c716472a06b4bd82418e70e6deee5
tuan anh <tuananh@MacBook-Pro-cua-tuan.local>
2022-05-20 16:12:14 +0700

    [A] section 3

---
+++ Udemy/test/test2/iampoor/iampoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iampoor
+//
+//  Created by tuan anh on 20/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
