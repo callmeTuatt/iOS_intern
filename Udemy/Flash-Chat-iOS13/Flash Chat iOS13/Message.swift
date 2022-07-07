123eb8182b9e1f0925c785cf3cf09662eb04c4de
tuan-anh-mk1 <trananh16112001@gmail.com>
2022-07-07 22:35:02 +0700

    [A] Section 15

---
+++ Udemy/Flash-Chat-iOS13/Flash Chat iOS13/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by tuan anh on 05/07/2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message{
+    let sender: String
+    let body: String
+    
+}
